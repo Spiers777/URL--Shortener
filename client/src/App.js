54a5b868bf4f7a9b6b7d0e59528d5aa455f57bc8
Isaac Spiers <isaacspiers7@gmail.com>
2023-06-02 21:37:51 +0100

    client upload
    deleted .git file

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import './App.scss';
+
+function App() {
+  const [backendData, setBackendData] = useState(null)
+  const [URL, setURL] = useState("")
+
+  const handleURLcreation = async () => {
+    fetch(`/api/create?url=${URL}`).then(res => res.json()).then(data => {
+      setBackendData(data)
+    })
+  }
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(`${backendData.server}/${backendData.extension}`)
+  }
+
+  return (
+    <div className='container'>
+      <h1>URL Shortener</h1>
+      <fieldset className='URLinput'>
+        <input type="url" placeholder="www.somewhere.com" onChange={e => setURL(e.target.value)}/>
+        <button onClick={handleURLcreation}>Shorten URL</button>
+      </fieldset>
+      <h2 className='outputLink' onClick={copyLink}>{backendData ? `${backendData.server}/${backendData.extension}` : ""}</h2>
+    </div>
+  )
+}
+
+export default App
