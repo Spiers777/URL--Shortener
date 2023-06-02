24c6b60b2d954c515820717f3c419679ce139030
Isaac Spiers <isaacspiers7@gmail.com>
2023-06-02 22:32:29 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const {MongoClient} = require('mongodb');
-const {cors} = require('cors')
+const cors = require('cors')
 require('dotenv').config()
 
 const password = process.env.DB_PASSWORD
